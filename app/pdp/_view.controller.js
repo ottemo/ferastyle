9223f36d67dd1734de3b791fa772b5111a321f98
vsilvestrov <vsilvestrov@speroteck.com>
2016-09-29 12:35:38 +0300

    [#130455085] PDP styling is done (#6)

---
+++ app/pdp/_view.controller.js
@@ -0,0 +1,42 @@
+angular.module('pdpModule')
+
+    .controller('pdpViewController', [
+        '$controller',
+        '$scope',
+        '$location',
+        'pdpApiService',
+        'pdpProductService',
+        function ($controller, $scope, $location, pdpApiService, pdpProductService) {
+
+            // Inherit from default pdpViewController
+            $controller('_pdpViewController', { $scope: $scope });
+
+            $scope.selectImage = function (newSrc) {
+                $scope.activeImg = newSrc;
+            };
+
+            $scope._getProduct = function () {
+                pdpApiService.getProduct({'productID': $scope.productId}).$promise.then(function (response) {
+                    if (response.error === null) {
+                        var result = response.result || $scope.defaultProduct;
+
+                        pdpProductService.setProduct(result);
+                        $scope.product = pdpProductService.getProduct();
+
+                        // BREADCRUMBS
+                        $scope.$emit('add-breadcrumbs', {
+                            'label': $scope.product.name,
+                            'url': pdpProductService.getUrl($scope.product._id)
+                        });
+
+                        if (angular.isArray($scope.product.images) &&  $scope.product.images[0]) {
+                            $scope.activeImg = $scope.product.images[0].large;
+                        }
+                    } else {
+                        $location.path('/');
+                    }
+                });
+            };
+        }
+    ]
+);
