ec14e7ca528070f55c9a57ff75592bc416716396
vsilvestrov <vsilvestrov@speroteck.com>
2016-10-13 18:33:18 +0300

    Vs 131632719 add initializing to the slider after ng repeat event (#18)
    * the slider for small images on PDP was added
    
    * [#131632719] add initializing to the slider after ng-repeat event

--- app/pdp/_view.controller.js
+++ app/pdp/_view.controller.js
@@ -12,7 +12,7 @@ angular.module('pdpModule')
             $controller('_pdpViewController', { $scope: $scope });
 
             $scope.selectImage = function (newSrc) {
-                $scope.activeImg = newSrc;
+                $scope.activeImg = newSrc;git 
             };
 
             $scope._getProduct = function () {
@@ -22,6 +22,7 @@ angular.module('pdpModule')
 
                         pdpProductService.setProduct(result);
                         $scope.product = pdpProductService.getProduct();
+                        $scope.$broadcast('product.loaded');
 
                         // BREADCRUMBS
                         $scope.$emit('add-breadcrumbs', {
