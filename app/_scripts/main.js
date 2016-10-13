156b5bfc19f3297d0cb7dd9620a96070bf3c6efd
vsilvestrov <vsilvestrov@speroteck.com>
2016-10-13 15:02:59 +0300

    1. Update dropdown items in top nav bar 2. Create a button for immediately going up to the top of the site 3. Product info page - add two modal window for size charts 4. create modal for sizing guid button on PDP and Quick View 5. add sale prices and strike through on Search Result page (#16)

--- app/_scripts/main.js
+++ app/_scripts/main.js
@@ -42,4 +42,22 @@ $(document).ready(function () {
         }
     });
 
+    // adult size charts modal for "product-info" cms page
+    $(document).on('click','#myBtn',function(event){
+        $('#popup_adult_chart').modal('show');
+    });
+
+    // youth size charts modal for "product-info" cms page
+    $(document).on('click','#yourchar',function(event){
+        $('#popup_youth_chart').modal('show');
+    })
+
+    // size charts modal for "product-info" cms page
+    $(document).on('click', '#option-chart', function(e) {
+        $('#modal_adult_chart').modal('show');
+    });
+
+
+
+
 });
