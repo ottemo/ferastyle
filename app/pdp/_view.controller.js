57a1d98024dd129642d0e3385f55b07bf7b51049
vsilvestrov <vsilvestrov@speroteck.com>
2016-10-19 17:30:49 +0300

    [#130453359] PDP - Browse next/previous item in category is done (#21)

--- app/pdp/_view.controller.js
+++ app/pdp/_view.controller.js
@@ -6,13 +6,35 @@ angular.module('pdpModule')
         '$location',
         'pdpApiService',
         'pdpProductService',
-        function ($controller, $scope, $location, pdpApiService, pdpProductService) {
+        "categoryApiService",
+        "commonRewriteService",
+        "$rootScope",
+        function ($controller, $scope, $location, pdpApiService, pdpProductService, categoryApiService, commonRewriteService, $rootScope) {
 
             // Inherit from default pdpViewController
             $controller('_pdpViewController', { $scope: $scope });
 
+            $scope.activate = function() {
+                // REFACTOR: why store options in a service instead of on the
+                // product, idk
+                // BUG: if we don't clean out the options the pdpProductService tries to hold onto those
+                // options on page change
+                pdpProductService.setOptions({});
+                $scope.ratingInfo = $scope._getDefaultRatingInfo();
+
+                $scope._getProduct();
+                $scope._getReviews();
+                $scope._getRatingInfo();
+                $scope._initWatchers();
+                $scope.getPdpProductsList();
+            };
+
+            $scope.productButtonsAvailable = false;
+
+            $scope.productService = pdpProductService;
+
             $scope.selectImage = function (newSrc) {
-                $scope.activeImg = newSrc;git 
+                $scope.activeImg = newSrc;
             };
 
             $scope._getProduct = function () {
@@ -38,6 +60,58 @@ angular.module('pdpModule')
                     }
                 });
             };
+
+            $scope.getPdpProductsList = function (){
+                if ($rootScope.productsListUpperLevel) {
+                    $scope.productsList = $rootScope.productsListUpperLevel;
+                    getPdpProductsLinks();
+                }
+                else {
+                    var categotyName = $location.absUrl().split('/')[3];
+                    var parentCategoryId = commonRewriteService.getCategoryIdByUrl(categotyName);
+                    if(parentCategoryId !== null) {
+                        categoryApiService.getProductsByCategoryId({categoryID: parentCategoryId}).$promise
+                            .then(function (response) {
+                                if(response.result !== null) {
+                                    $rootScope.productsListUpperLevel = response.result || [];
+                                    $scope.productsList = $rootScope.productsListUpperLevel;
+                                    getPdpProductsLinks();
+                                }
+                            });
+                    }
+                }
+            };
+
+            function getPdpProductsLinks() {
+                var currentPosition = -1;
+                if ($scope.productsList.length > 0) {
+                    for (var i=0; i < $scope.productsList.length; i++) {
+                        var product = $scope.productsList[i];
+                        if (product._id === $scope.productId) {
+                            currentPosition = i;
+                            break;
+                        }
+                    }
+                    if (currentPosition < $scope.productsList.length - 1) {
+                        $scope.nextPositionId = $scope.productsList[currentPosition + 1]["_id"];
+                        $scope.nextPositionImg = $scope.productsList[currentPosition + 1]["image"]["thumb"];
+                    }
+                    if (currentPosition === $scope.productsList.length - 1) {
+                        $scope.nextPositionId = $scope.productsList[0]["_id"];
+                        $scope.nextPositionImg = $scope.productsList[0]["image"]["thumb"];
+                    }
+                    if (currentPosition > 0) {
+                        $scope.previousPositionId = $scope.productsList[currentPosition - 1]["_id"];
+                        $scope.previousPositionImg = $scope.productsList[currentPosition - 1]["image"]["thumb"];
+                    }
+                    if (currentPosition === 0) {
+                        $scope.previousPositionId = $scope.productsList[$scope.productsList.length - 1]["_id"];
+                        $scope.previousPositionImg = $scope.productsList[$scope.productsList.length - 1]["image"]["thumb"];
+                    }
+                    $scope.productButtonsAvailable = true;
+                }
+            }
+
         }
     ]
 );
