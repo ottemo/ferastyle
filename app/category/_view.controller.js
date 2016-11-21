a95a23e5fe14d494dd5d7e343939caba11710301
Dmitry Glushenkov <dglushenkov@speroteck.com>
2016-11-21 12:06:20 -0500

    configurable products - quick view

--- app/category/_view.controller.js
+++ app/category/_view.controller.js
@@ -149,6 +149,127 @@ angular.module('categoryModule')
                 $scope.filters[filterKey] = {};
                 $scope.filters[filterKey][filterValue] = true;
             }
+        };
+
+        /**
+         * Opens quick view modal window
+         */
+        $scope.openPopUp = function(product) {
+            $scope.message = {};
+            $scope.options = {};
+            pdpProductService.setProduct(product);
+            var productPromise = pdpProductService.getProduct();
+            var mediaConfigPromise = mediaService.getMediaConfig();
+            $scope.inStock = product.qty !== 0;
+
+            $q.all([productPromise, mediaConfigPromise]).then(function(responses) {
+                $scope.popupProduct = responses[0];
+                var mediaConfig = responses[1];
+                $scope.productService.getRatingInfo(product._id);
+                $scope.swatches = getSwatches(product, mediaConfig);
+            });
+
+
+            $("#quick-view").modal('show');
+            setTimeout(function() {
+                try {
+                    $('.rating').rating('update', $scope.productService.getAverageRating());
+                } catch (e) {
+
+                }
+            }, 300);
+        };
+
+        /**
+         * Builds product swatches from options
+         * result example {
+             *      colors: {
+             *          black: {
+             *              swatchImageUrl: 'demo.ottemo.io/media/swatch/colors_black.png',
+             *              imageUrl: 'demo.ottemo.io/media/image/Product/123/black.jpg',
+             *              label: 'Black",
+             *              selected: false,
+             *              _ids: [1, 2, 7, 10]
+             *          },
+             *          red: {
+             *            ...
+             *          }
+             *      },
+             *      sizes: {
+             *          ...
+             *      }
+             * }
+         */
+        function getSwatches(product, mediaParams) {
+            var swatches = {};
+            var size = 'medium';
+            _.forEach(product.options, function(option) {
+                if (option.type === 'select_image' || option.type === 'select_text') {
+                    var swatchSet = {};
+                    var subOptions = option.options;
+                    _.forEach(subOptions, function(selection) {
+                        var swatch = {};
+                        swatch.label = selection.label;
+                        swatch.selected = false;
+                        if (option.type === 'select_image') {
+                            swatch.swatchImageUrl = mediaService
+                                .getSwatchImage(option.key, selection.key, mediaParams);
+                        }
+                        if (option.controls_image === true && selection.image_name) {
+                            swatch.imageUrl = mediaService
+                                .getProductImage(product._id, selection.image_name, size, mediaParams);
+                        }
+                        if (option.has_associated_products === true) {
+                            swatch._ids = selection._ids;
+                        }
+
+                        swatchSet[selection.key] = swatch;
+                    });
+
+                    if (!_.isEmpty(swatchSet)) {
+                        swatches[option.key] = swatchSet;
+                    }
+                }
+            });
+
+            if (!_.isEmpty(swatches)) {
+                removeSoldOutProductIds(product, swatches);
+            }
+            return swatches;
+        }
+
+        /**
+         *  Removes sold out product ids from the swatches
+         */
+        function removeSoldOutProductIds(product, swatches) {
+            var inventory = product.inventory;
+            _.forEach(inventory, function(inventoryItem) {
+                var inventoryOptions = inventoryItem.options;
+                if (_.isEmpty(inventoryOptions) || inventoryItem.qty !== 0) return;
+
+                var _ids = [];
+                _.forEach(inventoryOptions, function(optionValue, optionKey) {
+                    var productOption = _.find(product.options, { key: optionKey.toString() });
+
+                    if (productOption && productOption.options) {
+                        var selection = _.find(productOption.options, { key: optionValue.toString() });
+
+                        if (selection && selection._ids && selection._ids.length > 0) {
+                            _ids.push(selection._ids);
+                        }
+                    }
+                });
+
+                var soldOutIds = _.intersection.apply(this, _ids);
+                if (soldOutIds.length > 0) {
+                    _.forEach(inventoryOptions, function(optionValue, optionKey) {
+                        if (swatches[optionKey] && swatches[optionKey][optionValue]) {
+                            var swatch = swatches[optionKey][optionValue];
+                            swatch._ids = _.difference(swatch._ids, soldOutIds);
+                        }
+                    })
+                }
+            });
         }
     }
 ]);
