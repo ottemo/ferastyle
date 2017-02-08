7f3f0f5fc33093b024032c9afd368ed2f56b1133
Dmitry Glushenkov <dglushenkov@speroteck.com>
2017-02-08 16:48:06 +0200

    [#139187321] - support anchor navigation on cms pages (#53)

--- app/common/_init.js
+++ app/common/_init.js
@@ -3,6 +3,17 @@ angular.module('commonModule')
 // SEO Meta Data
 .value('DEFAULT_TITLE', 'Ferastyle')
 
+.run(['$rootScope', '$location', '$anchorScroll', '$timeout',
+    function($rootScope, $location, $anchorScroll, $timeout) {
+        $rootScope.$on('cfpLoadingBar:completed', function() {
+            $timeout(function() {
+                if($location.hash()) {
+                    $anchorScroll();
+                }
+            }, 0);
+        });
+}])
+
 .config([
     '$routeProvider',
     function(
